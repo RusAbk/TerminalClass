6e846026c5e2eb3b10ddfc9d225f89dd7ecf986a
Ruslan Abkadirov <rusabk@yandex.ru>
2020-12-13 17:36:12 +0300

    main.cpp rewritten using TD class

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
+#include "td.h"
+
+using namespace std;
 
 int main() {
-  std::cout << "Hello World!\n";
+    TD::setStyle(TD::italic);
+    TD::setColors(1, 9);
+    cout << "Hello, World!" << endl;
+    TD::reset();
+    TD::goHome();
 }
